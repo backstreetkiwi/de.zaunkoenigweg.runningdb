dcdea451ffd489fd4b66aa0d3ac444b351c65d3c
backstreetkiwi <mail@nikolaus-winter.de>
2013-10-09 13:38:27 +0200

    accordion panel in recordui (ugly so far)

--- src/de/zaunkoenigweg/runningdb/ui/RecordsUi.java
+++ src/de/zaunkoenigweg/runningdb/ui/RecordsUi.java
@@ -6,11 +6,14 @@ import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
 
+import com.vaadin.ui.Accordion;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.FormLayout;
+import com.vaadin.ui.Panel;
 import com.vaadin.ui.Table;
+import com.vaadin.ui.Table.Align;
 import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
 
 import de.zaunkoenigweg.runningdb.domain.RecordDistance;
 import de.zaunkoenigweg.runningdb.domain.RecordInfo;
@@ -28,13 +31,41 @@ public class RecordsUi extends AbstractUi {
 
     private static final long serialVersionUID = 3021578951350704103L;
     
-    private static final int NUMBER_OF_RUNS_PER_RECORD_DISTANCE = 3;
+    private static final int NUMBER_OF_RUNS_PER_RECORD_DISTANCE = 10;
 
-    private static final DateFormat DATE_FORMATTER = new SimpleDateFormat("dd.MM.yyyy");    
+    private static final DateFormat DATE_FORMATTER = new SimpleDateFormat("dd.MM.yyyy");
+    
+    private Accordion accordion;
 
     Button buttonAddRecordDistance;
     
     public RecordsUi() {
+
+      VerticalLayout layout = new VerticalLayout();
+      layout.setMargin(true);
+      setCompositionRoot(layout);
+      
+      this.accordion = new Accordion();
+      layout.addComponent(this.accordion);
+
+      // button "add new record distance"
+      buttonAddRecordDistance = new Button("Strecke hinzufügen");
+      layout.addComponent(buttonAddRecordDistance);
+
+      buttonAddRecordDistance.addClickListener(new Button.ClickListener() {
+          
+          private static final long serialVersionUID = 8949212155808289711L;
+
+          @Override
+          public void buttonClick(Button.ClickEvent event) {
+              
+              // create and show window to add new record distance
+              // TODO: add listener to window
+              RecordDistanceInputWindow bestzeitStreckeEingabe = new RecordDistanceInputWindow(RecordsUi.this);
+              UI.getCurrent().addWindow(bestzeitStreckeEingabe);
+          }
+      });
+
     }
 
     @Override
@@ -46,35 +77,35 @@ public class RecordsUi extends AbstractUi {
      * Builds/Refreshes UI.
      */
     private void refreshUi() {
+        
+        this.accordion.removeAllComponents();
 
-        FormLayout layout = new FormLayout();
-        layout.setMargin(true);
-        setCompositionRoot(layout);
+        Panel panel = null;
+        VerticalLayout panelLayout = null;
+        String caption = "";
+        
+        List<RecordInfo> records = getTrainingLog().getRecords();
 
-        // one table per record distance is added to ui
-        List<RecordInfo> bestzeiten = getTrainingLog().getRecords();
-        for (RecordInfo bestzeitInfo : bestzeiten) {
-            layout.addComponent(createBestzeitTable(bestzeitInfo));
-            layout.addComponent(createButtonRemoveBestzeit(bestzeitInfo));
+        for (RecordInfo recordInfo : records) {
+            
+            String distance = new DistanceConverter().convertToPresentation(recordInfo.getRecordDistance().getDistance(), String.class, null);
+            if (StringUtils.isNotBlank(recordInfo.getRecordDistance().getLabel())) {
+                caption = String.format("%s: %s Meter (%d mal gelaufen)", recordInfo.getRecordDistance().getLabel(), distance, recordInfo.getRunCount());
+            } else {
+                caption = String.format("%s Meter (%d mal gelaufen)", distance, recordInfo.getRunCount());
+            }
+
+            panel = new Panel();
+            panelLayout = new VerticalLayout();
+            panel.setContent(panelLayout);
+            
+            panelLayout.addComponent(createBestzeitTable(recordInfo));
+            panelLayout.addComponent(createButtonRemoveBestzeit(recordInfo));
+            
+            this.accordion.addTab(panel, caption);
+            
         }
         
-        // button "add new record distance"
-        buttonAddRecordDistance = new Button("Strecke hinzufügen");
-        layout.addComponent(buttonAddRecordDistance);
-
-        buttonAddRecordDistance.addClickListener(new Button.ClickListener() {
-            
-            private static final long serialVersionUID = 8949212155808289711L;
-
-            @Override
-            public void buttonClick(Button.ClickEvent event) {
-                
-                // create and show window to add new record distance
-                // TODO: add listener to window
-                RecordDistanceInputWindow bestzeitStreckeEingabe = new RecordDistanceInputWindow(RecordsUi.this);
-                UI.getCurrent().addWindow(bestzeitStreckeEingabe);
-            }
-        });
     }
 
     /**
@@ -84,34 +115,30 @@ public class RecordsUi extends AbstractUi {
      */
     private Table createBestzeitTable(RecordInfo recordInfo) {
 
-        String caption = "";
-        String distance = new DistanceConverter().convertToPresentation(recordInfo.getRecordDistance().getDistance(), String.class, null);
-        if (StringUtils.isNotBlank(recordInfo.getRecordDistance().getLabel())) {
-            caption = String.format("%s: %s Meter (%d mal gelaufen)", recordInfo.getRecordDistance().getLabel(), distance, recordInfo.getRunCount());
-        } else {
-            caption = String.format("%s Meter (%d mal gelaufen)", distance, recordInfo.getRunCount());
-        }
+        Table table = new Table();
 
-        Table table = new Table(caption);
-
+        table.addContainerProperty("rank", Integer.class, null);
         table.addContainerProperty("date", String.class, null);
         table.addContainerProperty("time", Integer.class, null);
         table.addContainerProperty("pace", Integer.class, null);
+        table.setColumnHeader("rank", "");
         table.setColumnHeader("date", "Datum");
         table.setColumnHeader("time", "Zeit");
         table.setColumnHeader("pace", "Schnitt");
         table.setConverter("time", new TimeConverter());
         table.setConverter("pace", new TimeConverter());
         table.setSortEnabled(false);
+        table.setColumnWidth("rank", 25);
         table.setColumnWidth("date", 100);
         table.setColumnWidth("time", 100);
         table.setColumnWidth("pace", 100);
+        table.setColumnAlignment("rank", Align.CENTER);
 
         List<RecordRun> runs = recordInfo.getRecordRuns();
         int i = 0;
         while (i < NUMBER_OF_RUNS_PER_RECORD_DISTANCE && i < runs.size()) {
             RecordRun lauf = runs.get(i);
-            table.addItem(new Object[] {DATE_FORMATTER.format(lauf.getTraining().getDate()), lauf.getTime(), RunningDbUtil.getPace(recordInfo.getRecordDistance().getDistance(), lauf.getTime()) }, null);
+            table.addItem(new Object[] {(i+1), DATE_FORMATTER.format(lauf.getTraining().getDate()), lauf.getTime(), RunningDbUtil.getPace(recordInfo.getRecordDistance().getDistance(), lauf.getTime()) }, null);
             i++;
         }
         table.setPageLength(NUMBER_OF_RUNS_PER_RECORD_DISTANCE);
